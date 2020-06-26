c9ad18043257266328a17706aa6e5b1f3a0f651b
henaime <henaime@sqli.com>
2020-06-26 14:19:31 +0100

    pdp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,40 +5,46 @@ import { AppComponent } from './app.component';
 import { translations, translationChunksConfig } from '@spartacus/assets';
 import { B2cStorefrontModule } from '@spartacus/storefront';
 import { Cart2Component } from './cart2/cart2.component';
-import {CustomRoutingModule} from './custom-routing/custom-routing.module';
+import { CustomRoutingModule } from './custom-routing/custom-routing.module';
+import { CustomPdpModule } from './custom-pdp/custom-pdp.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Cart2Component
-  ],
+  declarations: [AppComponent, Cart2Component],
   imports: [
     BrowserModule,
+
     B2cStorefrontModule.withConfig({
       backend: {
         occ: {
-          baseUrl: 'https://local.medi-market.be:9002',
-          prefix: '/rest/v2/'
-        }
+          baseUrl: 'https://medicaremarket.local:9002',
+          prefix: '/mcmcomspartacus/v2/',
+        },
       },
       context: {
-        urlParameters : ['baseSite', 'language', 'currency'],
-        baseSite : ['medimarket-be'],
+        //urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['medimarket-be'],
         currency: ['EUR'],
         language: ['fr'],
       },
       i18n: {
         resources: translations,
         chunks: translationChunksConfig,
-        fallbackLang: 'en'
+        fallbackLang: 'en',
       },
       features: {
-        level: '2.0'
-      }
+        level: '2.0',
+      },
+      routing: {
+        routes: {
+          product: { paths: ['product/:productCode'] },
+          cart: { paths: ['cart'] },
+        },
+      },
     }),
-    CustomRoutingModule
+    CustomRoutingModule,
+    CustomPdpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/cart2/cart2.component.ts
+++ src/app/cart2/cart2.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {ActiveCartService, AuthService, OccEndpointsService, OrderEntry, UserOrderService, UserService, UserToken} from '@spartacus/core';
-import {Observable} from 'rxjs';
-import {take} from 'rxjs/operators';
+import {
+  ActiveCartService,
+  AuthService,
+  OccEndpointsService,
+  OrderEntry,
+  UserOrderService,
+  UserService,
+  UserToken,
+} from '@spartacus/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 // import {UserAuthenticationTokenService} from '@spartacus/core/src/auth/services/user-authentication/user-authentication-token.service';
 
 @Component({
   selector: 'app-cart2',
   templateUrl: './cart2.component.html',
-  styleUrls: ['./cart2.component.scss']
+  styleUrls: ['./cart2.component.scss'],
 })
 export class Cart2Component implements OnInit {
-
   orderEntries$: Observable<OrderEntry[]> = this.cartService.getEntries();
 
-  constructor(private cartService: ActiveCartService,
-              private userService: UserService,
-              private authService: AuthService) { }
+  constructor(
+    private cartService: ActiveCartService,
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
   login() {
-    this.authService.authorize('fredo1994007@gmail.com_mm', 'Mcm123');
+    this.authService.authorize('hamza.enaime@outlook.com_mm', 'P@ss123MCM');
   }
   loginForm() {
-
     const uid = (document.getElementById('uid') as HTMLInputElement).value;
     const pass = (document.getElementById('pass') as HTMLInputElement).value;
     // @ts-ignore
@@ -34,7 +40,6 @@ export class Cart2Component implements OnInit {
     this.authService.logout();
   }
   randomEntries() {
-    this.cartService.addEntry('1002', 5);
-    this.cartService.addEntry('1003', 15);
+    this.cartService.addEntry('1000', 5);
   }
 }
